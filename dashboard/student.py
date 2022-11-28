f2d51f7507d6900e46211a2e5b1391e7d277aa12
Handrian_W <69663236+HandrianW@users.noreply.github.com>
2022-11-28 08:50:13 +0700

    add getAll, update, and delete
    menyesuaikan dengan database team

--- dashboard/student.py
+++ dashboard/student.py
@@ -11,6 +11,11 @@ api = Api(bp)
 
 class Students(Resource):
     @jwt_required()
+    def get(self):
+        data = getAll_student()
+        return json.loads(dumps(data))
+    
+    @jwt_required()
     def post(self):
         req = request.form
         data = {
@@ -47,5 +52,48 @@ class Student(Resource):
         filter = {'_id' : ObjInstance}
         data = get_student(filter)
         return json.loads(dumps(data))
+    
+    @jwt_required()
+    def put(self, student_id):
+        ObjInstance = ObjectId(student_id)
+        filter = {'_id':ObjInstance}
+        if (get_student(filter) is None):
+            return {"message":"ID not valid"}
+        else:
+            req = request.form
+            name = req.get("nama")
+            newVal = {
+                "$set":{
+                    'nama' : req['nama'],
+                    'jenis_kelamin' : req['jenis_kelamin'], 
+                    'nisn' : req['nisn'], 
+                    'nik' : req['nik'], 
+                    'no_kk' : req['no_kk'], 
+                    'tingkat_kelas' : req['tingkat_kelas'], 
+                    'id_rombongan_belajar' : int(req['id_rombongan_belajar']), 
+                    'tanggal_masuk' :req['tanggal_masuk'],
+                    'tanggal_lulus' :req['tanggal_lulus'],
+                    'nomor_induk' : req['nomor_induk'],
+                    'status' : req['status'],
+                    'tinggi_badan' : int(req['tinggi_badan']),
+                    'berat_badan' : int(req['berat_badan']),
+                    'lingkar_kepala' : int(req['lingkar_kepala']),
+                    'alergi' : req['alergi'],
+                    'nama_ayah' : req['nama_ayah'],
+                    'nama_ibu' : req['nama_ibu'],
+                    'pekerjaan_ayah' : req['pekerjaan_ayah'],
+                    'pekerjaan_ibu' : req['pekerjaan_ibu'],
+                }
+            }
+
+            update_student(filter, newVal)
+            return {"success":True}
+
+    @jwt_required()
+    def delete(self, student_id):
+        ObjInstance = ObjectId(student_id)
+        filter = {'_id':ObjInstance}
+        data = delete_student(filter)
+        return {"success":True}
 
 api.add_resource(Student, '/API/students/<student_id>')
